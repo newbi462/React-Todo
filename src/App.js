e539d30bedde722bbf72a77dc2367436e5ecc59f
newbi462 <isthisemailfree@gmail.com>
2019-11-28 16:24:08 -0500

    Adds to list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -8,17 +8,24 @@ class App extends React.Component {
       list: [0,1,2],
       typing: [],
       void: [],
+      placeHolder: <input placeholder="New List Item" onChange={this.typeListItem} />,
     };
-  }
-
+  };
 
   // design `App` to be the parent component of your application.
 
 
   // this component is going to take care of state, and any change handlers you need to work with your state
-  newlistItem = foobarBannaEvent => {
-    this.setState({list: [...this.state.list, foobarBannaEvent.target.value]});
+  typeListItem = foobarBannaEvent => {
+    this.setState({typing: [foobarBannaEvent.target.value]});
   };
+  newListItem = foobarBannaEvent => {
+    this.setState({list: [...this.state.list, this.state.typing[0]]});
+    this.setState({typing: []});
+    this.setState({placeHolder: <input placeholder="New List Item" onChange={this.typeListItem} />});
+    this.state.placeHolder.input.reset();
+  };
+
 
 
   render() {
@@ -31,9 +38,11 @@ class App extends React.Component {
         {this.state.list.map(function(item) {
           return <li>{item}</li>;
         })}
+        {this.state.typing[0]}
         </ul>
 
-        <input onChange={this.newlistItem} />
+        {this.state.placeHolder}
+        <button onClick={this.newListItem}>Add to List</button>
       </div>
       </>
     );
